953bedd8a12e5be462e515256d70a0565c448a3f
Kowalski25 <it.felixarhelacbang@gmail.com>
2024-06-03 16:25:32 +0800

    Wrap app with redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes/Routes";
 import { Footer, Header } from "./components";
+import { Provider } from "react-redux";
+import store from "./context/store";
 
 const App = () => {
   return (
-    <Router>
-      {/* Shared component across the app */}
-      <div className='container'>
-        <Header />
-
-        {/* Application routes */}
-        <Routes />
-
-        {/* Shared component across the app */}
-        <Footer />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes />
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
   );
 };
 
